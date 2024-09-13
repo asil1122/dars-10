23143235cae67cf0d22aacdf5cd958d3a12d68f3
asil1122 <asilbekabdurashidov000gmail.com>
2024-09-13 13:21:25 +0500

    new

--- client/js/main.js
+++ client/js/main.js
@@ -1,63 +1,37 @@
 const box = document.querySelector(".box");
 const form = document.querySelector(".form");
-const inputs = document.querySelectorAll(".inputs");
+const input = document.querySelector(".inputs");
+import { getInfo, getPosts, getId } from "./service.js";
 
 
-const render = async (data) => {
-    box.innerHTML = data.map(
-      (item) => ` <div class="box_2">
-      <h1 class = "title">${item.title}</h1>
-      <button data-id="${item.id}" class="btn_del">delete</button>
-      <button data-edit = "${item.id}" class= "btn_edit">edit</button>
-      </div>
-    `
-    ).join("");
-  };
-  
-  const getData = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/new_products");
-      const data = await res.json();
-      await render(data);
-    } catch (error) {
-        return error.message
-    }
-  };
-  
-  getData();
-  
-  box.addEventListener("click", async (e) => {
-    const deleteItem = e.target.dataset.id;
-    if (deleteItem) {
-      try {
-        const res = await fetch(`http://localhost:3000/new_products/${deleteItem}`, {
-          method: "DELETE",
-        });
-        await getData(res);
-      } catch (error) {
-        return error.message
-    }
-    }
-  });
-  
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    let obj = {};
-    for (let i of inputs) {
-      obj[i.name] = i.value;
-      i.value = "";
-    }
-    try {
-      const res = await fetch("http://localhost:3000/new_products", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(obj),
-      });
-      await getData(res);
-    } catch (error) {
-        return error.message
-    }
-  });
-  
+
+const renderInput = async () => {
+  const data = await getPosts();
+  box.innerHTML = data
+    .map(
+      (item) => `
+      <div class="input_text_block">
+      <h1 class="title_in">${item.title}</h1>
+      <button class="input_inner_del" data-id="${item.id}">Delete</button>
+      <button class="input_inner_btn a" data-id="${item.id}">Edit</button>
+      </div>`
+    )
+    .join("");
+};
+renderInput();
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  await getInfo({ title: input.value });
+  input.value = "";
+
+  renderInput();
+});
+
+box.addEventListener("click", async (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    await getId(id);
+  }
+  renderInput()
+});
--- client/js/service.js
+++ client/js/service.js
@@ -1,39 +1,37 @@
+const url = "http://localhost:3000";
 
-// export const getData = async () => {
-//     try {
-//       const res = await fetch("http://localhost:3000/new_products");
-//       const data = await res.json();
-//     return data
-//     } catch (error) {
-//         return error.message
-//     }
-//   };
-  
+export const getPosts = async () => {
+  try {
+    const res = await fetch(`${url}/input`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+};
 
-// export const getId = async () => {
-//     try {
-//       const res = await fetch(`http://localhost:3000/new_products/${id}`, {
-//         method: "DELETE",
-//       });
-//     const data = await res.json()
-//     return data
-//     } catch (error) {
-//         return error.message
-//     }
-// }
+export const getInfo = async (dataForm) => {
+  try {
+    const res = await fetch(`${url}/input`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(dataForm),
+    });
+  } catch (error) {
+    return error.message;
+  }
+};
 
-// export const getdata = async () => {
-//     try {
-//      const res = await fetch("http://localhost:3000/new_products", {
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           method: "POST",
-//           body: JSON.stringify(data),
-//         });
-//     const data = await res.json()
-//     return data
-//       } catch (error) {
-//         return error.message
-//     }
-// }
+export const getId = async (deleteItemId) => {
+  try {
+    const res = await fetch(`${url}/input/${deleteItemId}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+};
